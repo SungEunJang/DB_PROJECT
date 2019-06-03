be3d60f6ca517d5b70fd11440dba371d6bec2c3e
SungEunJang <suuuu101@naver.com>
2019-06-03 21:12:05 +0900

    닉네임 중복방지

--- registration.php
+++ registration.php
@@ -76,37 +76,52 @@
 		$sinfo_mail = $_POST['email'];
 		$sinfo_nick = $_POST['nick'];
 
-		if ($_POST['password'] != $_POST['passconfirm'])
+		if (strlen($_POST['password']) < 5)
+			echo "<script>alert('Your passwords is too short'); history.back();</script>";
+		else if ($_POST['password'] != $_POST['passconfirm'])
 			echo "<script>alert('Your passwords do not match'); history.back();</script>";
 		else {
 	
 			try {
-				$stmt = $con->prepare(
-					'INSERT INTO SINFO(sinfo_name, sinfo_ssn, sinfo_major, sinfo_pass, sinfo_mail, sinfo_nick) 
-					VALUES(:sinfo_name, :sinfo_ssn, :sinfo_major, :sinfo_pass, :sinfo_mail, :sinfo_nick)'
-				);
 
-				$stmt->bindparam(':sinfo_name', $sinfo_name);
-				$stmt->bindparam(':sinfo_ssn', $sinfo_ssn);
-				$stmt->bindparam(':sinfo_major', $sinfo_major);
-				$stmt->bindparam(':sinfo_pass', $sinfo_pass);
-				$stmt->bindparam(':sinfo_mail', $sinfo_mail);
-				$stmt->bindparam(':sinfo_nick', $sinfo_nick);
+				$st_mt = $con->prepare("SELECT sinfo_nick FROM SINFO WHERE sinfo_nick = '$sinfo_nick'");
+				$st_mt->bindparam(':sinfo_nick', $sinfo_nick);
+				$st_mt->execute();
+				$row = $st_mt->fetch();
+				$is_nick_exists = $row['sinfo_nick'];
 
-				if ($stmt->execute()) {
-					unset($_SESSION['user_name']);
-					unset($_SESSION['user_major']);
-					unset($_SESSION['user_ssn']);
-					echo '<script>alert("Registration Success!");</script>';
-					echo "<script>location.href='index.php'</script>";
-				}
+				if ($is_nick_exists)
+					echo "<script>alert('$sinfo_nick Already exists. Try another nickname');</script>";
 				else {
-					echo '<script>alert("Err");</script>';
+
+					$stmt = $con->prepare(
+						'INSERT INTO SINFO(sinfo_name, sinfo_ssn, sinfo_major, sinfo_pass, sinfo_mail, sinfo_nick) 
+						VALUES(:sinfo_name, :sinfo_ssn, :sinfo_major, :sinfo_pass, :sinfo_mail, :sinfo_nick)'
+					);
+
+					$stmt->bindparam(':sinfo_name', $sinfo_name);
+					$stmt->bindparam(':sinfo_ssn', $sinfo_ssn);
+					$stmt->bindparam(':sinfo_major', $sinfo_major);
+					$stmt->bindparam(':sinfo_pass', $sinfo_pass);
+					$stmt->bindparam(':sinfo_mail', $sinfo_mail);
+					$stmt->bindparam(':sinfo_nick', $sinfo_nick);
+
+					if ($stmt->execute()) {
+						unset($_SESSION['user_name']);
+						unset($_SESSION['user_major']);
+						unset($_SESSION['user_ssn']);
+						echo '<script>alert("Registration Success!");</script>';
+						echo "<script>location.href='index.php'</script>";
+					}
+					else {
+						echo '<script>alert("Err");</script>';
+					}
 				}
 
 			} catch(PDOException $e) {
 	            die("Database error: " . $e->getMessage()); 
 	        }
+
     	}
 	}
 ?>
